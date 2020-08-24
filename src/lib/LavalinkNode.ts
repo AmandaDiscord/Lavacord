3c70e3179b98fd49e25816366e378fb22659eaae
Papi <bradleyjplatt@yahoo.com>
2020-08-24 03:56:49 -0700

    Destorys ur process

--- src/lib/LavalinkNode.ts
+++ src/lib/LavalinkNode.ts
@@ -159,12 +159,12 @@ export class LavalinkNode {
     }
 
     /**
-     * Destorys the connection to the Lavalink Websocket
+     * Destroys the connection to the Lavalink Websocket
      */
     public destroy(): boolean {
         if (!this.connected) return false;
         this.ws!.close(1000, "destroy");
-        this.ws = null;
+        // this.ws = null;
         return true;
     }
 
